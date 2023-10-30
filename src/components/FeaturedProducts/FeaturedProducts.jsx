54540b3c6dfbfe165e856852c63165e196484b60
ARABY Mohamed Ext O-EG/HRCS <mgmz5673@orange.com>
2023-10-30 15:52:25 +0200

    Display Products

---
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import style from './FeaturedProducts.module.css'
+import axios from 'axios'
+
+export default function FeaturedProducts() {
+  const [products,setProducts] = useState([])
+  async function getProducts(){
+    let data = await axios.get(`https://ecommerce.routemisr.com/api/v1/products`);
+    setProducts(data.data.data)
+  }
+  useEffect(()=>{
+    getProducts();
+
+  },[])
+  return <>
+
+  <div className='row'>
+    {products.map((product)=> <div key={product._id} className='col-md-2'>
+    <div className='product px-2 py-4 cursor-pointer'>
+      <img className='w-100' src={product.imageCover} alt=''/>
+      <span className='text-main fw-bolder font-sm'>{product.category.name}</span>
+      <h3 className='h6 fw-bolder'>{product.title.split(' ').slice(0,2).join(' ')}</h3>
+      <div className='d-flex justify-content-between'>
+        <span className='text-muted'>{product.price} EGP</span>
+        <span>
+          <i className='fas fa-star rating-color'></i>
+          {product.ratingsAverage}
+        </span>
+
+        </div>
+        <button className='btn bg-main text-white w-100'>+ Add</button>
+    </div>
+    </div>
+    )}
+  </div>
+
+    </>
+  
+}
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import style from './Home.module.css'
+import FeaturedProducts from '../FeaturedProducts/FeaturedProducts'
 export default function Home() {
   return <>
 
+
+        <FeaturedProducts/>
         <h2>Home</h2>
     </>
   
